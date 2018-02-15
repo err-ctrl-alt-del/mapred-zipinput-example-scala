09e9ca885c93f7a886d20f1f31322f9031b100a7
gerrard-err <gerrard.errappa@gmail.com>
2018-02-15 15:16:30 +0200

    Improved test

--- src/test/scala/com/github/mapred/example/MapReduceTestCase.scala
+++ src/test/scala/com/github/mapred/example/MapReduceTestCase.scala
@@ -4,44 +4,50 @@ import java.io.File
 
 import org.junit.Assert._
 import org.hamcrest.CoreMatchers._
-import org.junit.{Before, Test}
+import org.junit.{After, Before, Test}
 import junit.framework.TestCase
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{LocalFileSystem, Path}
-import org.apache.hadoop.mrunit.mapreduce.{MapDriver, MapReduceDriver, ReduceDriver}
 import com.cotdp.hadoop.ZipFileInputFormat
 import com.google.common.io.Resources
-import org.apache.hadoop.mapreduce.TaskAttemptID
+import org.apache.hadoop.io.{BytesWritable, IntWritable, Text}
+import org.apache.hadoop.mapreduce.{RecordReader, TaskAttemptID}
 import org.apache.hadoop.mapreduce.lib.input.FileSplit
 import org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl
+import org.apache.hadoop.mrunit.mapreduce.MapDriver
 import org.apache.hadoop.util.ReflectionUtils
 
 class MapReduceTestCase extends TestCase {
 
-  private val mapper = new CompressedFileInputMapper
-  private val reducer = new CompressedFileInputReducer
-
-  val mapDriver = MapDriver.newMapDriver(mapper)
-  val reduceDriver = ReduceDriver.newReduceDriver(reducer)
-  val mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer)
+  private val mapDriver: MapDriver[Text, BytesWritable, Text, IntWritable] =
+    MapDriver.newMapDriver(new CompressedFileInputMapper)
+  private val conf: Configuration = createConfiguration()
+  private var inputFormat: ZipFileInputFormat = _
+  private var reader: RecordReader[Text, BytesWritable] = _
 
   @Before
   @throws[Exception]
-  override def setUp(): Unit = {}
+  override def setUp(): Unit = {
+    mapDriver.clearInput()
+    inputFormat = ReflectionUtils.newInstance(classOf[ZipFileInputFormat], conf)
+  }
 
-  @Test
-  def testMapper() {
+  def createConfiguration(): Configuration = {
     val conf = new Configuration(false)
     conf.set("fs.file.impl", classOf[LocalFileSystem].getName)
     conf.set("fs.defaultFS", "file:///")
-    val testFile = new File(Resources.getResource("test.zip").getFile())
+    conf
+  }
+
+  @Test
+  def testMapper() {
+    val testFile = new File(Resources.getResource("test.zip").getFile)
     val path = new Path(testFile.getAbsoluteFile.toURI)
     val split = new FileSplit(path, 0, testFile.length, null)
     val inputFormat = ReflectionUtils.newInstance(classOf[ZipFileInputFormat], conf)
     val context = new TaskAttemptContextImpl(conf, new TaskAttemptID())
-    val reader = inputFormat.createRecordReader(split, context)
+    reader = inputFormat.createRecordReader(split, context)
     reader.initialize(split, context)
-    mapDriver.setMapper(new CompressedFileInputMapper)
     while ( {
       reader.nextKeyValue
     }) {
@@ -51,6 +57,12 @@ class MapReduceTestCase extends TestCase {
     assertNotNull(result)
     assertTrue(result.size() == 1)
     assertThat(result.toString, containsString("test.csv"))
+  }
+
+  @After
+  @throws[Exception]
+  override def tearDown(): Unit = {
+    reader.close()
   }
 
 }
